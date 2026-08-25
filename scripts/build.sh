90d6a12fb93cfb96931a575269b59e423309f331
gajo-luccas <luccashenrydacar@gmail.com>
2026-08-25 17:23:06 -0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -92,7 +92,7 @@ prepare_defconfig() {
 # ----------------------------------------------------------------- build ---
 
 make_args() {
-	printf '%s' "O=out ARCH=${ARCH}"
+	printf '%s' "ARCH=${ARCH}"
 	[ -n "${CUSTOM_CMDS:-}" ] && printf ' %s' "$CUSTOM_CMDS"
 	[ -n "${EXTRA_CMDS:-}"  ] && printf ' %s' "$EXTRA_CMDS"
 	[ -n "${GCC_64:-}"      ] && printf ' %s' "$GCC_64"
