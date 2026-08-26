da1c24189465e46ac95ae97f8210dfc5d7e150b1
gajo-luccas <luccashenrydacar@gmail.com>
2026-08-26 10:25:27 -0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -142,7 +142,7 @@ sed -i 's|#include <spi-xiaomi-tp.h>|#include "spi-xiaomi-tp.h"|' drivers/input/
 
 info "make ${args}"
 # shellcheck disable=SC2086
-make -j1 CC="$cc" KCFLAGS="-I$(pwd)/drivers/hid" $args \
+make -j1 CC="$cc" KCFLAGS="-I$(pwd)/drivers/hid -I$(pwd)/drivers/media/platform/msm/camera/cam_core" $args \
     || die "kernel build failed"
 
 	endgroup
