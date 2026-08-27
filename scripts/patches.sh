6b0c36f05c876d6544765e9e494a1743fdbb9d1a
gajo-luccas <luccashenrydacar@gmail.com>
2026-08-27 20:37:41 -0300

    Update patches.sh

--- scripts/patches.sh
+++ scripts/patches.sh
@@ -434,7 +434,6 @@ if [ "${BASH_SOURCE[0]}" = "${0}" ]; then
 			fi
 
 			if is_true "${ENABLE_SUSFS:-false}";      then susfs_apply;      fi
-			sed -i 's/if ($(1)) >\/dev\/null 2>\&1;/if ($(1));/' "${KERNEL_DIR}/scripts/Kbuild.include"
 			fix_vayu_xdp_buff
 			if is_true "${ENABLE_HIDE_STUFF:-false}"; then hide_stuff_apply; fi
 			;;
