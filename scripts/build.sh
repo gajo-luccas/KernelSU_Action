9336d2badb0a4175c2fd060ac55dc834b29d401b
gajo-luccas <luccashenrydacar@gmail.com>
2026-08-28 00:10:17 -0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -144,6 +144,7 @@ info "make ${args}"
 sed -i 's/#include <cam_sync_api.h>/#include "cam_sync_api.h"/' \
   drivers/media/platform/msm/camera/cam_sync/cam_sync_util.h
 sed -i 's#>/dev/null 2>\&1##' scripts/Kbuild.include
+clang --target=aarch64-linux-gnu -Werror -fstack-protector-strong -c -x c /dev/null -o /tmp/stacktest.o
 make -j2 CC="$cc" KCFLAGS="-I$(pwd)/drivers/hid -I$(pwd)/drivers/media/platform/msm/camera/cam_core -I$(pwd)/drivers/media/platform/msm/camera/cam_req_mgr -I$(pwd)/drivers/media/platform/msm/camera/cam_isp/isp_hw_mgr -I$(pwd)/drivers/media/platform/msm/camera/cam_sensor_module/cam_cci -I$(pwd)/drivers/media/platform/msm/camera/cam_sensor_module/cam_sensor_utils -I$(pwd)/drivers/media/platform/msm/camera/cam_utils -I$(pwd)/drivers/platform/msm/ipa/ipa_v3" $args || die "kernel build failed"
 
 	endgroup
